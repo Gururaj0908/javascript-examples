e568ec1fc28b4cc79147f7d9f1ba35fef8f647b0
gururaj0908 <gururaj0324@gmail.com>
2024-02-15 13:05:42 +0530

    first commit

---
+++ example.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b =10;
+let c =a+b;
+let d =String(c);
+let e="hello world";
+let f="123";
+let g= true;
+let h=Number(e);
+let i= Number(f);
+let j=Boolean(b);
+
+console.log(c);
+console.log(d);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(typeof(g));
+
+
